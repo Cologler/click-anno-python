f977f462098bbdb44e791032ad0ed9e055061e19
Cologler <skyoflw@gmail.com>
2019-04-02 13:44:38 +0800

    update version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description_content_type='text/markdown',
     packages=find_packages(),
     name='click_anno',
-    version='0.0.1',
+    version='0.0.2',
     description='',
     keywords=['click'],
     author='Cologler',
